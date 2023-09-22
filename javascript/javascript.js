f2868f2730dc5bd0a2b6a1408ff58ffdd55cf715
bigavilanezb00 <bigavilanezb00@elpuig.xeill.net>
2023-09-22 20:55:37 +0200

    exercicio 2 empezado

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,10 +2,13 @@
 function myFunction() {
     let dobles = "I'm = I ama";
     let simples = 'You\'re = You are'
-    document.getElementById("exercici-1").innerHTML = dobles + "\n" + simples;
+    document.getElementById("exercici-1").innerHTML = dobles + "<br>" + simples;
 }
 
 //Exercici 2
 function myFunction2(){
-
+    const valor = 23;
+    if (typeof valor === 'number') {
+        console.log('Es un número');
+    }
 }
