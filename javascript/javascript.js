5bed146af94ded52430f0353b00411b41d54515c
bigavilanezb00 <bigavilanezb00@elpuig.xeill.net>
2023-09-22 20:46:15 +0200

    first commit

---
+++ javascript/javascript.js
@@ -0,0 +1,11 @@
+//Exercici 1
+function myFunction() {
+    let dobles = "I'm = I ama";
+    let simples = 'You\'re = You are'
+    document.getElementById("exercici-1").innerHTML = dobles + "\n" + simples;
+}
+
+//Exercici 2
+function myFunction2(){
+
+}
